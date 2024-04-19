5d88a75d9cf47fbe83ffed02cd326762f1eee387
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2024-04-19 17:54:57 -0300

    add invoice and invoiceItem models

---
+++ src/modules/invoice/repository/invoice.model.ts
@@ -0,0 +1,35 @@
+import {
+  Column,
+  Model,
+  PrimaryKey,
+  Table,
+  HasMany
+} from "sequelize-typescript";
+import InvoiceItemModel from "./invoiceItem.model";
+
+@Table({
+  tableName: 'invoice',
+  timestamps: false,
+})
+export default class InvoiceModel extends Model {
+  @PrimaryKey
+  @Column({ allowNull: false })
+  id: string;
+
+  @Column({ allowNull: false })
+  name: string;
+
+  @Column({ allowNull: false })
+  document: string;
+
+  @Column({ allowNull: false })
+  createdAt: Date;
+
+  @Column({ allowNull: false })
+  updatedAt: Date;
+
+  @HasMany(() => InvoiceItemModel)
+  invoiceItems: ReturnType<() => InvoiceItemModel[]>;
+
+  // TODO: Add address association
+}
---
+++ src/modules/invoice/repository/invoiceItem.model.ts
@@ -0,0 +1,35 @@
+import {
+  AutoIncrement,
+  BelongsTo,
+  Column,
+  ForeignKey,
+  Model,
+  PrimaryKey,
+  Table
+} from "sequelize-typescript";
+import InvoiceModel from "./invoice.model";
+
+@Table({
+  tableName: 'invoiceItems',
+  timestamps: false,
+})
+export default class InvoiceItemModel extends Model {
+  @PrimaryKey
+  @Column({ allowNull: false })
+  id: string;
+
+  @Column({ allowNull: false })
+  name: string;
+
+  @Column({ allowNull: false })
+  price: string;
+
+  @ForeignKey(() => InvoiceModel)
+  @Column({ allowNull: false })
+  invoiceId: string;
+
+  // I had some problem with using InvoiceModel directly.
+  // GitHub issue: https://github.com/sequelize/sequelize-typescript/issues/825#issuecomment-1176225028
+  @BelongsTo(() => InvoiceModel)
+  invoice: ReturnType<() => InvoiceModel>;
+}
